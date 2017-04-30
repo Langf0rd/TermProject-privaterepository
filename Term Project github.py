a24ed916e179aee86201343cc2181aeb0ae77c38
Langf0rd <vmitta@andrew.cmu.edu>
2017-04-30 11:58:42 -0400

    added splash screens and score

--- Term Project github.py
+++ Term Project github.py
@@ -10,98 +10,147 @@ from tkinter import *
 import math
 import random
 
-####################################
-# customize these functions
-####################################
-
 def init(data):
     data.cenX = data.width/2
     data.cenY = data.height/2
-    data.mode = "playing"
-    if data.mode == "playing":
-        initPlaying(data)
+    data.mode = "title"
     data.cubeSpread = 20000
-    data.deathSpread = 1000
+    data.deathSpread = 1200
     data.cubeDist = 200000
+    data.cubeWidth = 2000
     data.timerCount = 0
     data.speed = 3000
+    data.movSpeed = 200
     data.cubeRate = 3
+    data.playerPos = [0, 0, 1500, 0, 0]
+    data.cubes = []
+    data.score = 0
 
 def drawDead(canvas, data):
-    canvas.create_text(data.width/2, data.height/2, text = "Game Over")
+    canvas.create_text(data.width/2, data.height/2, 
+                        text = "Game Over", font = "Arial 25 bold")
+    canvas.create_text(data.width/2, data.height/1.5,
+                        text = "Press 'space' to restart", font = "Arial 15")
 
 def removePassed(data):
     if data.cubes[0].getCenter()[1] < data.playerPos[1]:
         data.cubes.pop(0)
 
 def checkDeath(data):
-    if ((abs(data.cubes[0].getCenter()[0] - data.playerPos[0]) < data.deathSpread) and 
-        (abs(data.cubes[0].getCenter()[1] - data.playerPos[1]) <= data.deathSpread) and
-        (abs(data.cubes[0].getCenter()[2] - data.playerPos[2]) < data.deathSpread)):
+    if ((abs(data.cubes[0].getCenter()[0] - data.playerPos[0]) 
+                < data.deathSpread) and 
+        (abs(data.cubes[0].getCenter()[1] - data.playerPos[1]) 
+                <= data.speed) and
+        (abs(data.cubes[0].getCenter()[2] - data.playerPos[2]) 
+                < data.deathSpread)):
         data.mode = "dead"
 
-def initPlaying(data):
-    data.playerPos = [0, 0, 1500, 0, 0]
-    data.cubes = []
+def drawTitle(canvas, data):
+    canvas.create_text(data.width/2, data.height/5, 
+                text = "Welcome To CubeRunner", font = "Arial 20 bold")
+    canvas.create_text(data.width/2, data.height/2,
+                text = "To start playing, press 'space'", 
+                font = "Arial 15 bold")
+    canvas.create_text(data.width/2, data.height/1.5,
+                text = "For help press 'H'", 
+                font = "Arial 15 bold")
 
 def drawCubes(canvas, data):
     for i in data.cubes:
         i.draw(canvas, data)
 
 def generateCubes(data):
-    data.cubes.append(cube(data.playerPos[0] + random.randint(-data.cubeSpread, data.cubeSpread),
-                        data.playerPos[1] + data.cubeDist, 0, 2000))
+    data.cubes.append(cube(data.playerPos[0] + 
+                        random.randint(-data.cubeSpread, data.cubeSpread),
+                        data.playerPos[1] + data.cubeDist, 0, data.cubeWidth))
 
 def mousePressed(event, data):
     # use event.x and event.y
     pass
 
 def keyPressed(event, data):
+    if data.mode == "playing":
+        playingKeyPressed(event, data)
+    if data.mode == "title":
+        titleKeyPressed(event, data)
+    if data.mode == "dead":
+        deadKeyPressed(event, data)
+
+def playingKeyPressed(event, data):
+    # Need to change so its when pressed not single press
     if event.keysym == "w":
-        data.playerPos[1] += 100*math.cos(math.radians((data.playerPos[3])))
-        data.playerPos[0] += 100*math.sin(math.radians((data.playerPos[3]))) # Need to change so its when pressed not single press
+        data.playerPos[1] += data.movSpeed*math.cos(math.radians((data.playerPos[3])))
+        data.playerPos[0] += data.movSpeed*math.sin(math.radians((data.playerPos[3])))
     if event.keysym == "s":
-        data.playerPos[1] += 100*math.cos(math.radians((data.playerPos[3] + 180)))
-        data.playerPos[0] += 100*math.sin(math.radians((data.playerPos[3] + 180)))
+        data.playerPos[1] += data.movSpeed*math.cos(math.radians((data.playerPos[3] + 180)))
+        data.playerPos[0] += data.movSpeed*math.sin(math.radians((data.playerPos[3] + 180)))
     if event.keysym == "d":
-        data.playerPos[1] += 100*math.cos(math.radians((data.playerPos[3] + 270)))
-        data.playerPos[0] += 100*math.sin(math.radians((data.playerPos[3] + 270)))
+        data.playerPos[1] += data.movSpeed*math.cos(math.radians((data.playerPos[3] + 270)))
+        data.playerPos[0] += data.movSpeed*math.sin(math.radians((data.playerPos[3] + 270)))
     if event.keysym == "a":
-        data.playerPos[1] += 100*math.cos(math.radians((data.playerPos[3] + 90)))
-        data.playerPos[0] += 100*math.sin(math.radians((data.playerPos[3] + 90)))
-    if event.keysym == "Up":
-        if data.playerPos[4] < 60:
-            data.playerPos[4] += 1 # Need to change it to the mouse movement
-    if event.keysym == "Down":
-        if data.playerPos[4] > -60:
-            data.playerPos[4] -= 1
-    if event.keysym == "Left":
-        data.playerPos[3] += 1
-        if data.playerPos[3] == 90 or data.playerPos[3] == 270:
-            data.playerPos[3] += 1
-    if event.keysym == "Right":
-        data.playerPos[3] -= 1
-        if data.playerPos[3] == 90 or data.playerPos[3] == 270:
-            data.playerPos[3] -= 1
+        data.playerPos[1] += data.movSpeed*math.cos(math.radians((data.playerPos[3] + 90)))
+        data.playerPos[0] += data.movSpeed*math.sin(math.radians((data.playerPos[3] + 90)))
+    #if event.keysym == "Up":
+    #    if data.playerPos[4] < 60:
+    #        data.playerPos[4] += 1 # Need to change it to the mouse movement
+    #if event.keysym == "Down":
+    #    if data.playerPos[4] > -60:
+    #        data.playerPos[4] -= 1
+    #if event.keysym == "Left":
+    #    data.playerPos[3] += 1
+    #    if data.playerPos[3] == 90 or data.playerPos[3] == 270:
+    #        data.playerPos[3] += 1
+    #if event.keysym == "Right":
+    #    data.playerPos[3] -= 1
+    #    if data.playerPos[3] == 90 or data.playerPos[3] == 270:
+    #        data.playerPos[3] -= 1
+
+def titleKeyPressed(event, data):
+    if event.keysym == "space":
+        data.mode = "playing"
+    if event.keysym == "h":
+        data.mode = "help"
+
+def deadKeyPressed(event, data):
+    if event.keysym == "space":
+        init(data)
 
 def timerFired(data):
-    data.timerCount += 1
-    if data.timerCount == data.cubeRate:
-        data.timerCount = 0
-    if data.timerCount == 1:
-        generateCubes(data)
-    data.playerPos[1] += data.speed
-    removePassed(data)
-    checkDeath(data)
+    if data.mode == "playing":
+        data.timerCount += 1
+        if data.timerCount == data.cubeRate:
+            data.timerCount = 0
+        if data.timerCount == 1:
+            generateCubes(data)
+        data.score += (data.speed//1000)
+        data.speed += 10
+    if data.mode == "playing" or data.mode == "dead":
+        data.playerPos[1] += data.speed
+        removePassed(data)
+        checkDeath(data)
     if data.mode == "dead":
         data.speed = data.speed / 1.3
 
 def redrawAll(canvas, data):
-    drawCubes(canvas, data)
-    drawCrosshairs(canvas, data)
+    if data.mode == "playing" or data.mode == "dead":
+        drawCubes(canvas, data)
+        drawScore(canvas, data)
+        drawCrosshairs(canvas, data)
     if data.mode == "dead":
         drawDead(canvas, data)
+    elif data.mode == "title":
+        drawTitle(canvas, data)
+    elif data.mode == "help":
+        drawHelp(canvas, data)
 
+def drawHelp(canvas, data):
+    canvas.create_text(data.width/2, data.height/5, 
+                        text = "help")
+
+def drawScore(canvas, data):
+    canvas.create_text(data.width/6, data.height/8, 
+                        text = "Score = "+str(data.score), font = "Arial 15",
+                        anchor = "w")
 
 def drawCrosshairs(canvas, data):
     canvas.create_rectangle(data.cenX - 30, data.cenY - 30,
@@ -110,7 +159,8 @@ def drawCrosshairs(canvas, data):
 
 def getBend(offset, distance, point, data):
     xBen = (offset[0]/(distance/1200))
-    yBen = (offset[1]/((((distance**2) - ((data.playerPos[2] - point[2])**2))**0.5)/1200))
+    yBen = (offset[1]/((((distance**2) - 
+            ((data.playerPos[2] - point[2])**2))**0.5)/1200))
     x = (data.playerPos[0] - point[0])
     y = (data.playerPos[1] - point[1])
     return (xBen, yBen)
@@ -171,21 +221,30 @@ class cube(object):
                         self.face4, self.face5)
     def draw(self, canvas, data, colour = "grey"):
         for i in self.faces:
-            canvas.create_polygon(data.cenX + getBend((getOffset(i[0], data.playerPos)), getDistance(i[0], data.playerPos), i[0], data)[0], 
-                            data.cenY + getBend((getOffset(i[0], data.playerPos)), getDistance(i[0], data.playerPos), i[0], data)[1],
-                            data.cenX + getBend((getOffset(i[1], data.playerPos)), getDistance(i[1], data.playerPos), i[1], data)[0],
-                            data.cenY + getBend((getOffset(i[1], data.playerPos)), getDistance(i[1], data.playerPos), i[1], data)[1],
-                            data.cenX + getBend((getOffset(i[2], data.playerPos)), getDistance(i[2], data.playerPos), i[2], data)[0],
-                            data.cenY + getBend((getOffset(i[2], data.playerPos)), getDistance(i[2], data.playerPos), i[2], data)[1],
-                            data.cenX + getBend((getOffset(i[3], data.playerPos)), getDistance(i[3], data.playerPos), i[3], data)[0],
-                            data.cenY + getBend((getOffset(i[3], data.playerPos)), getDistance(i[3], data.playerPos), i[3], data)[1],
+            canvas.create_polygon(
+                data.cenX + getBend((getOffset(i[0], data.playerPos)), 
+                    getDistance(i[0], data.playerPos), i[0], data)[0], 
+                data.cenY + getBend((getOffset(i[0], data.playerPos)), 
+                    getDistance(i[0], data.playerPos), i[0], data)[1],
+                data.cenX + getBend((getOffset(i[1], data.playerPos)), 
+                    getDistance(i[1], data.playerPos), i[1], data)[0],
+                data.cenY + getBend((getOffset(i[1], data.playerPos)), 
+                    getDistance(i[1], data.playerPos), i[1], data)[1],
+                data.cenX + getBend((getOffset(i[2], data.playerPos)), 
+                    getDistance(i[2], data.playerPos), i[2], data)[0],
+                data.cenY + getBend((getOffset(i[2], data.playerPos)), 
+                    getDistance(i[2], data.playerPos), i[2], data)[1],
+                data.cenX + getBend((getOffset(i[3], data.playerPos)), 
+                    getDistance(i[3], data.playerPos), i[3], data)[0],
+                data.cenY + getBend((getOffset(i[3], data.playerPos)), 
+                    getDistance(i[3], data.playerPos), i[3], data)[1],
                             fill = colour, outline = colour)
     def getCenter(self):
         return (self.x1 + (self.length/2), self.y1 + (self.length/2),
                     self.z1 + (self.length/2))
 
 ####################################
-# use the run function as-is
+# Run Function
 ####################################
 
 def run(width=300, height=300):
@@ -230,4 +289,4 @@ def run(width=300, height=300):
     root.mainloop()  # blocks until window is closed
     print("bye!")
 
-run(600, 600)
+run(720, 720)
