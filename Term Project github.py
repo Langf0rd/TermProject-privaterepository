8ca3d1c9f2dd26b5c05d8f4967b1e61fbedb61a8
Langf0rd <vmitta@andrew.cmu.edu>
2017-05-03 11:27:46 -0400

    Finished i think
    commented , changed settings a little, organised

--- Term Project github.py
+++ Term Project github.py
@@ -15,44 +15,45 @@ import random
 #####################################
 
 def init(data):
-    data.cenX = data.width/2
+    data.cenX = data.width/2 # Gets the center of the screen x and y values
     data.cenY = data.height/2
     initSettings(data)
     initDefaults(data)
     initHelp(data)
 
+# Initialises values that do not change during gameplay
 def initSettings(data):
     data.cubeSpread = 20000
-    data.deathSpread = 1400
+    data.deathSpread = 1600 # This distance from cube center = collision
     data.cubeDist = 200000
     data.cubeWidth = 2000
-    data.movSpeed = 1000
-    data.cubeRate = 3
+    data.movSpeed = 800
+    data.cubeRate = 3 # Determines how often a cube is generated
     data.startHeight = 1500
-    data.gravity = -100
-    data.antigravity = 100
-    data.lookReturn = 0.1
+    data.gravity = -100 # Used to return the player to ground level when jumping
+    data.antigravity = 100 # Same as above but used for sliding
+    data.lookReturn = 0.1 # Used to pull the camera view back to center
+    # data.colours and data.cubeHeights are used to randomize the cubes
     data.colours = ("yellow", "blue", "green", "dark blue", 
                     "light blue", "light green", "maroon", "red")
     data.cubeHeights = (0, 0, 0, 0, 0, 0, 1500, 1500)
-    data.cubeWidths = (data.cubeWidth, data.cubeWidth, data.cubeWidth, 
-                        data.cubeWidth, data.cubeWidth*2, data.cubeWidth*2, 
-                        data.cubeWidth*3)
 
+# Initialises values that will be changed during gameplay
 def initDefaults(data):
     data.score = 0
     data.timerCount = 0
     data.mode = "title"
     data.playerPos = [0, 0, data.startHeight, 0, 0]
-    data.right = False
+    data.right = False # Is the player moving right/left
     data.left = False
-    data.scored = 0
-    data.cubes = []
+    data.scored = 0 
+    data.cubes = [] # List is updated with all the cubes on screen
     data.jumpVel = 0
     data.lookVel = 0
     data.slideVel = 0
     data.speed = 3000
 
+# Initialises everything used for the help screen
 def initHelp(data):
     data.redHelpCube = cube(-5000, 45000, 1500, data.cubeWidth, "red")
     data.darkRedHelpCube = cube(-6000, 35000, 1400, data.cubeWidth, "maroon")
@@ -62,6 +63,8 @@ def initHelp(data):
 # Action and gameplay functions
 #####################################
 
+# For jump and slide, when the Vel values are changed the gravity values
+# pull the player back towards the ground
 def jump(data):
     data.jumpVel = 600
 
@@ -69,10 +72,24 @@ def slide(data):
     data.lookVel = -0.5
     data.slideVel = -500
 
+def moveRight(data):
+    data.playerPos[1] += (data.movSpeed+data.speed/50)*math.cos(math.radians(
+                                                (data.playerPos[3] + 270)))
+    data.playerPos[0] += (data.movSpeed+data.speed/50)*math.sin(math.radians(
+                                                (data.playerPos[3] + 270)))
+
+def moveLeft(data):
+    data.playerPos[1] += (data.movSpeed+data.speed/50)*math.cos(math.radians(
+                                                (data.playerPos[3] + 90)))
+    data.playerPos[0] += (data.movSpeed+data.speed/50)*math.sin(math.radians(
+                                                (data.playerPos[3] + 90)))
+
 def removePassed(data):
     if data.cubes[-1].getCenter()[1] < data.playerPos[1]:
         data.cubes.pop()
 
+# Checks if the player has collided with a cube and either sets to game over
+# or gives points accordingly
 def checkDeath(data):
     if ((abs(data.cubes[-1].getCenter()[0] - data.playerPos[0]) 
                 < data.deathSpread) and 
@@ -96,9 +113,12 @@ def generateCubes(data):
         colour = data.colours[select]
     data.cubes.insert(0, cube(data.playerPos[0] + 
                         random.randint(-data.cubeSpread, data.cubeSpread),
-                        data.playerPos[1] + data.cubeDist, height, data.cubeWidth, 
-                        colour))
+                        data.playerPos[1] + data.cubeDist, height, 
+                        data.cubeWidth, colour))
 
+# getBend is used to determing the 'bend' based on how far away an object is
+# from the player and then pushes the display of that object towards the center
+# of the screen to simulate objects in the distance appearing smaller
 def getBend(offset, distance, point, data):
     xBen = (offset[0]/(distance/1200))
     yBen = (offset[1]/((((distance**2) - 
@@ -112,6 +132,8 @@ def getDistance(a, b):
     temp2 = (temp1**2+(a[2]-b[2])**2)**0.5
     return temp2
 
+# Finds the left/right and up/down offset of an object from the player based on
+# both the players position and which direction the player is facing
 def getOffset(point, player):
     dist = getDistance(player, point)
     z = (player[2] - point[2])
@@ -145,37 +167,23 @@ def keyPressed(event, data):
             data.mode = "title"
 
 def playingKeyPressed(event, data):
-    # Need to change so its when pressed not single press
-    if event.keysym == "w":
-        data.playerPos[1] += data.movSpeed*math.cos(math.radians((data.playerPos[3])))
-        data.playerPos[0] += data.movSpeed*math.sin(math.radians((data.playerPos[3])))
-    #if event.keysym == "s":
-    #    data.playerPos[1] += data.movSpeed*math.cos(math.radians((data.playerPos[3] + 180)))
-    #    data.playerPos[0] += data.movSpeed*math.sin(math.radians((data.playerPos[3] + 180)))
+    # The left and right movement allows for roatation of the camera
+    # so if the player was not facing forward they would move left and right 
+    # based on where they were facing and not based on the maps x/y orientation
     if event.keysym == "Right" and data.playerPos[2] == data.startHeight:
-        data.playerPos[1] += data.movSpeed*math.cos(math.radians((data.playerPos[3] + 270)))
-        data.playerPos[0] += data.movSpeed*math.sin(math.radians((data.playerPos[3] + 270)))
+        data.playerPos[1] += data.movSpeed*math.cos(math.radians(
+                                            (data.playerPos[3] + 270)))
+        data.playerPos[0] += data.movSpeed*math.sin(math.radians(
+                                            (data.playerPos[3] + 270)))
     if event.keysym == "Left" and data.playerPos[2] == data.startHeight:
-        data.playerPos[1] += data.movSpeed*math.cos(math.radians((data.playerPos[3] + 90)))
-        data.playerPos[0] += data.movSpeed*math.sin(math.radians((data.playerPos[3] + 90)))
+        data.playerPos[1] += data.movSpeed*math.cos(math.radians(
+                                            (data.playerPos[3] + 90)))
+        data.playerPos[0] += data.movSpeed*math.sin(math.radians(
+                                            (data.playerPos[3] + 90)))
     if event.keysym == "Up" and data.playerPos[2] == data.startHeight:
         jump(data)
     if event.keysym == "Down" and data.playerPos[2] == data.startHeight:
         slide(data)
-    #if event.keysym == "Up":
-    #    if data.playerPos[4] < 60:
-    #        data.playerPos[4] += 1 # Need to change it to the mouse movement
-    #if event.keysym == "Down":
-    #    if data.playerPos[4] > -60:
-    #        data.playerPos[4] -= 1
-    #if event.keysym == "Left":
-    #    data.playerPos[3] += 1
-    #    if data.playerPos[3] == 90 or data.playerPos[3] == 270:
-    #        data.playerPos[3] += 1
-    #if event.keysym == "Right":
-    #    data.playerPos[3] -= 1
-    #    if data.playerPos[3] == 90 or data.playerPos[3] == 270:
-    #        data.playerPos[3] -= 1
 
 def keyDown(event, data):
     if event.keysym == "Right":
@@ -209,6 +217,7 @@ def deadKeyPressed(event, data):
 # Draw functions
 #####################################
 
+# Draws game over screen
 def drawDead(canvas, data):
     canvas.create_text(data.width/2, data.height/2, 
                         text = "Game Over", font = "Arial 25 bold")
@@ -225,19 +234,24 @@ def drawTitle(canvas, data):
                 text = "For help or instructions press 'H'", 
                 font = "Arial 15 bold")
 
+# Draws all the cubes in the list of current cubes
 def drawCubes(canvas, data):
     for i in data.cubes:
         i.draw(canvas, data)
 
+# Draws the indicators that tell the player they hit a yellow block
 def drawScored(canvas, data):
     canvas.create_rectangle(0,0, data.width, data.height, fill = "light yellow")
     canvas.create_text(data.width/2, data.height/5, text = "score + 100",
                         font = "Arial 20")
 
+# Draws the grey floor and horizon
 def drawHorizon(canvas, data):
-    data.horizon = cube(data.playerPos[0] - 100000, data.playerPos[1] + 230000, -180000, 180000, "light grey")
+    data.horizon = cube(data.playerPos[0] - 100000, data.playerPos[1] + 230000,
+                        -180000, 180000, "light grey")
     data.horizon.draw(canvas, data)
 
+# Draws the help screen
 def drawHelp(canvas, data):
     canvas.create_text(data.width/2, data.height/10, 
                         text = "Instructions", font = "Arial 15 bold")
@@ -245,8 +259,7 @@ def drawHelp(canvas, data):
                         text = "Welcome to CubeRunner!\n\nUse the arrow keys"+
                                 " to move around\n'Left' and 'Right' to "+
                                 "move side to side, 'Up' to jump and 'Down'"+
-                                " to slide\n\nNote: you can't move side to side"+
-                                " whilst you are in the air or sliding",
+                            " to slide",
                         font = "Arial 10", justify = "center")
     canvas.create_text(data.width/2, data.height/3,
                         text = ("Avoid cubes to stay alive for as long as" + 
@@ -268,10 +281,13 @@ def drawHelp(canvas, data):
                         text = "Press 'space' to return to menu",
                         font = "Arial 10 bold")
 
+# Draws the players score in the top left
 def drawScore(canvas, data):
     canvas.create_text(data.width/6, data.height/8, 
                         text = "Score = "+str(data.score), font = "Arial 15",
                         anchor = "w")
+
+# Used for testing to see how movement and scaling works
 
 #def drawCrosshairs(canvas, data):
 #    canvas.create_rectangle(data.cenX - 30, data.cenY - 30,
@@ -284,13 +300,28 @@ def drawScore(canvas, data):
 
 def timerFired(data):
     if data.mode == "playing":
-        data.timerCount += 1
-        if data.timerCount == data.cubeRate:
-            data.timerCount = 0
-        if data.timerCount == 1:
-            generateCubes(data)
-        data.score += (data.speed//1000)
-        data.speed += 10
+        playingTimerFired(data)
+    if data.mode == "playing" or data.mode == "dead":
+        data.playerPos[1] += data.speed
+        removePassed(data)
+        checkDeath(data)
+        if data.scored > 0:
+            data.scored -= 1
+    if data.mode == "dead":
+        data.speed = data.speed / 1.3
+    if data.left == True:
+        moveLeft(data)
+    if data.right == True:
+        moveRight(data)
+
+def playingTimerFired(data):
+    data.timerCount += 1
+    if data.timerCount == data.cubeRate:
+        data.timerCount = 0
+    if data.timerCount == 1:
+        generateCubes(data)
+    data.score += (data.speed//1000)
+    data.speed += 10
     if data.slideVel < 0 or data.playerPos[2] < data.startHeight:
         data.playerPos[2] += data.slideVel
     if data.slideVel < 1000:
@@ -303,20 +334,6 @@ def timerFired(data):
         data.playerPos[2] += data.jumpVel
     if data.jumpVel > -1000:
         data.jumpVel += data.gravity
-    if data.mode == "playing" or data.mode == "dead":
-        data.playerPos[1] += data.speed
-        removePassed(data)
-        checkDeath(data)
-        if data.scored > 0:
-            data.scored -= 1
-    if data.mode == "dead":
-        data.speed = data.speed / 1.3
-    if data.left == True and data.playerPos[2] == data.startHeight:
-        data.playerPos[1] += data.movSpeed*math.cos(math.radians((data.playerPos[3] + 90)))
-        data.playerPos[0] += data.movSpeed*math.sin(math.radians((data.playerPos[3] + 90)))
-    if data.right == True and data.playerPos[2] == data.startHeight:
-        data.playerPos[1] += data.movSpeed*math.cos(math.radians((data.playerPos[3] + 270)))
-        data.playerPos[0] += data.movSpeed*math.sin(math.radians((data.playerPos[3] + 270)))
 
 def redrawAll(canvas, data):
     if data.mode == "playing" or data.mode == "dead":
@@ -373,8 +390,10 @@ class cube(object):
                       (self.x1, self.y1, self.z2))
         self.faces = (self.face0, self.face1, self.face2, self.face3, 
                         self.face4, self.face5)
+    # Draws the cubes using their positions, offsets from the player
+    # and the 'bend' calculated from the distance they are from the player
     def draw(self, canvas, data):
-        for i in self.faces:
+        for i in self.faces: # draws each face of each cube
             canvas.create_polygon(
                 data.cenX + getBend((getOffset(i[0], data.playerPos)), 
                     getDistance(i[0], data.playerPos), i[0], data)[0], 
@@ -448,3 +467,36 @@ def run(width=720, height=720): # Default screen set to 720x720
     print("bye!")
 
 run(720, 720) # Screen size set to 720x720
+
+############################################
+# Spare Code
+############################################
+
+# Spare keypressed code that allows for rotation of the camera which I may
+# use in the future so I'm holding on to it
+
+#if event.keysym == "w":
+    #    data.playerPos[1] += data.movSpeed*math.cos(math.radians(
+    #                                                (data.playerPos[3])))
+    #    data.playerPos[0] += data.movSpeed*math.sin(math.radians(
+    #                                                (data.playerPos[3])))
+    #if event.keysym == "s":
+    #    data.playerPos[1] += data.movSpeed*math.cos(math.radians(
+    #                                                (data.playerPos[3] + 180)))
+    #    data.playerPos[0] += data.movSpeed*math.sin(math.radians(
+    #                                               (data.playerPos[3] + 180)))
+
+#if event.keysym == "Up":
+    #    if data.playerPos[4] < 60:
+    #        data.playerPos[4] += 1 # Need to change it to the mouse movement
+    #if event.keysym == "Down":
+    #    if data.playerPos[4] > -60:
+    #        data.playerPos[4] -= 1
+    #if event.keysym == "Left":
+    #    data.playerPos[3] += 1
+    #    if data.playerPos[3] == 90 or data.playerPos[3] == 270:
+    #        data.playerPos[3] += 1
+    #if event.keysym == "Right":
+    #    data.playerPos[3] -= 1
+    #    if data.playerPos[3] == 90 or data.playerPos[3] == 270:
+    #        data.playerPos[3] -= 1
